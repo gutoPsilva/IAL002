abe88d7273c0b381e00ecb737574b16bfd7b61b2
gutoPsilva <60670589+gutoPsilva@users.noreply.github.com>
2023-03-17 00:13:44 -0300

    pos-aula003

---
+++ Pós-aula 003/1001 - Extremamente Simples.py
@@ -0,0 +1,9 @@
+Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> A = int(input())
+10
+>>> B = int(input())
+9
+>>> X = A + B
+>>> print(f"X = {X}")
+X = 19
---
+++ Pós-aula 003/1002 - Área do Círculo.py
@@ -0,0 +1,8 @@
+Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> raio = float(input())
+2.00
+>>> pi = 3.14159
+>>> area = pi * (raio*raio)
+>>> print("A=%.4f" %area)
+A=12.5664
