c7b68428d423abad91fddad07e03366f4ade3720
gutoPsilva <60670589+gutoPsilva@users.noreply.github.com>
2023-06-04 16:59:00 -0300

    projeto 01 finalizado

--- Projeto 01 - Totalização Simples de Vendas de Produtos/N2A.py
+++ Projeto 01 - Totalização Simples de Vendas de Produtos/N2A.py
@@ -3,7 +3,7 @@ print("Vinicius Corrêa Carvalho")
 print("Projeto 01 - Totalização Simples de Vendas de Produtos\n")
 
 listaus = [] # armazena os codigos consultados não presentes no arquivo de texto mas dentro do intervalo
-listinval = [] # armazena os códigos inválidos do arquivo de texto
+listinval = [] # armazena os códigos inválidos do arquivo de texto, ou seja, códigos fora do intervalo
 listcod = [] # armazena todos os códigos válidos lidos
 listquant = [] # armazena todos as quantidades de venda lidas
 listvalor = [] # armazena todos os valores unitários lidos
@@ -25,7 +25,7 @@ arq.close()
 
 N = int(input("Digite o código a ser consultado: "))
 while N != 0:
-    if N < 10000 or N > 21000: # código consultado fora do intervalo
+    if N < 10000 or N > 21000:
         print("{} Código inválido (deve ser entre 10000 e 21000)".format(N))
     else: # se estiver dentro do intervalo
         i = 0 # zera a variável para uma nova consulta
@@ -37,7 +37,8 @@ while N != 0:
                 i = i + 1
             print("Total vendido do produto {} = R$ {:.2f}".format(N, total))
         else: # se o código não estiver na lista, significa que ele está dentro do intervalo mas não foi fornecido no arquivo de entrada, logo é ausente
-            listaus.append(N)
+            if N not in listaus:
+                listaus.append(N)
             print("Total vendido do produto {} = R$ 0.00".format(N))
 
     N = int(input("\nDigite o código a ser consultado: "))
